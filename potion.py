e2f4f56e2df5dbd9ba46f6473f8664c69f930cbb
Sharon Colson <sharon.colson@gmail.com>
2023-11-08 08:58:45 -0600

    if, elif, else statements in place; time to debug

--- potion.py
+++ potion.py
@@ -15,7 +15,7 @@ class Potion:
 
         # Uses internal method to get the potion name
         self.__name = self._get_potion_name()
-        self.__name = self._get_potion_value()
+        self.__value = self._get_potion_value()
 
     def get_name(self):
         return self.__name
@@ -32,7 +32,23 @@ class Potion:
         #     return "Failed Decoction"
         if(self.__ingredient1,self.__ingredient2) in [("Milk","Marinara Sauce"),("Marinara Sauce", "Milk")]:
             return "Potion of Regret"
+        elif(self.__ingredient1,self.__ingredient2) in [("Milk","Marinara Sauce"),("Marinara Sauce", "Milk")]:
+            return "Draught of Eavesdropping"
+        elif(self.__ingredient1,self.__ingredient2) in [("Milk","Marinara Sauce"),("Marinara Sauce", "Milk")]:
+            return "Philter of Amphibiosity"
+        elif(self.__ingredient1,self.__ingredient2) in [("Milk","Marinara Sauce"),("Marinara Sauce", "Milk")]:
+            return "Elixir of Halitosis"
         else:
             return "Failed Decoction"
+    
     def _get_potion_value(self):
-        return "Fix me"
+        if self.__name == "Elixir of Halitosis":
+            return random.randint(500,750)
+        elif self.__name == "Philter of Amphibiosity":
+            return random.randint(500,750)
+        elif self.__name == "Draught of Eavesdropping":
+            return random.randint(500,750)
+        elif self.__name == "Potion of Regret":
+            return random.randint(500,750)
+        else:
+            return 1
